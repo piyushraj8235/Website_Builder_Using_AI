e944da3d913485251d2adc7519306df88197722f
Piyush Raj <piyushrajgupta8235@gmail.com>
2026-04-12 03:45:18 +0530

    Update server URL in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import WebsiteEditor from './pages/Editor'
 import LiveSite from './pages/LiveSite'
 import Pricing from './pages/Pricing'
 
-export const serverUrl="https://website-builder-using-ai.onrender.com"
+export const serverUrl="https://website-builder-using-ai-1.onrender.com"
 function App() {
   useGetCurrentUser()
   const {userData}=useSelector(state=>state.user)
