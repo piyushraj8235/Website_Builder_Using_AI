12810ceabecbbaea2e747376a71d252c1650bbbe
piyushraj8235 <piyushrajgupta8235@gmail.com>
2026-04-17 02:43:18 +0530

    Update MongoDB connection config and fix DB connectivity

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose"
 
-const connectDb=async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-        console.log("db error")
-    }
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      family: 4,
+      serverSelectionTimeoutMS: 10000
+    });
+
+    console.log("MongoDB connected");
+
+  } catch (error) {
+    console.error("MongoDB error:", error.message);
+  }
 }
 
 export default connectDb
